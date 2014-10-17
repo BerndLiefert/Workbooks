c08215121051b8258369525dfa3d2545ec68403b
BerndLiefert <bernd.lauert.auf.kc@googlemail.com>
2014-10-17 15:28:35 +0200

    javascript workbook

---
+++ Workbook_JavaScript/js/site-old.js
@@ -0,0 +1,80 @@
+﻿
+
+function createpost(){
+//document.write("It works!");
+var title = $('#posttitle').val();
+var comment = $('#mypost').val();
+
+
+
+comment = setspan(comment);
+
+
+
+$('#wrapper').append('<div class="post"><h2><a id="' + title + '">' + title + '</a></h2>' + comment + '</div>');
+
+}
+
+
+
+
+
+function setspan(text){
+var keywords = ["function", "var", "log"];
+
+
+var regex = text.match(/"\S*"/gi);
+console.log(regex.length);
+
+
+if(regex){
+
+var unique = [];
+$.each(regex, function(i, el){
+    if($.inArray(el, unique) === -1) unique.push(el);
+});
+
+console.log(unique);
+
+
+/*
+//Dublikate entfernen
+for(var a = 0; a < regex.length; a++){
+var item = regex[a];
+	for(var b = 0; b < regex.length; b++){
+	 if(item = regex[b]){
+	 
+	 var test = regex.indexOf(item);
+	console.log('indexOf:'+ regex[test] + ' / ' + test);
+	 
+	  regex.splice(b, 1);
+	 }
+	}
+
+
+}
+*/
+
+//console.log(regex.length);
+
+
+for(var i=0; i<unique.length; i++){
+text = text.replace(unique[i], '<i><b>' + unique[i] + '</i></b>');
+console.log(unique[i]);
+	}
+}
+
+
+
+for(var i=0; i < keywords.length; i++){
+
+console.log(keywords[i]);
+text = text.replace(keywords[i], '<span class="keyword">' + keywords[i] + '</span>');
+console.log(text);
+}
+
+
+
+return text;
+
+}
---
+++ Workbook_JavaScript/js/site.js
@@ -0,0 +1,118 @@
+﻿
+
+function createpost(){
+//document.write("It works!");
+var title = $('#posttitle').val();
+var comment = $('#mypost').val();
+
+
+
+comment = setspan(comment);
+
+
+
+$('#wrapper').append('<div class="post"><h2><a id="' + title + '">' + title + '</a></h2>' + comment + '</div>');
+
+}
+
+
+
+
+
+function setspan(text){
+var keywords = ["abstract ", "arguments", "boolean", "break", "byte", "case", "catch",
+"char", "const", "continue", "debugger",
+"default", "delete", "do", "double", "else", "enum",
+"eval",
+"export",
+"extends",
+"false",
+"final",
+"finally",
+"float",
+"for",
+"function",
+"goto",
+"if",
+"implements",
+"import",
+"in",
+"instanceof",
+"int",
+"interface",
+"let",
+"long",
+"ative",
+"new",
+"null",
+"package",
+"private",
+"protected",
+"public",
+"return",
+"short",
+"static",
+"super",
+"switch",
+"synchronized",
+"this",
+"throw",
+"throws",
+"transient",
+"true",
+"try",
+"typeof",
+"var",
+"void",
+"volatile",
+"while",
+"with",
+"yield"];
+
+
+
+var regex = text.match(/"\S*"/gi);
+
+
+
+
+
+
+
+
+if(regex){
+
+var unique = [];
+$.each(regex, function(i, el){
+    if($.inArray(el, unique) === -1) unique.push(el);
+});
+
+console.log(unique);
+
+
+for(var i=0; i<unique.length; i++){
+
+var myreg = new RegExp(unique[i], 'g');
+text = text.replace(myreg, '<i><b>' + unique[i] + '</i></b>');
+console.log(unique[i]);
+	}
+}
+
+
+
+
+
+for(var i=0; i < keywords.length; i++){
+
+console.log(keywords[i]);
+
+var pattern = new RegExp(keywords[i], 'g');
+text = text.replace(pattern, '<span class="keyword">' + keywords[i] + '</span>');
+console.log(text);
+}
+
+
+
+return text;
+
+}
